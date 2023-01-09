90f3b9dcb18285af8a4ad8072475d69fa5fabf06
mohammad <smirzaie9287@gmail.com>
2023-01-09 21:23:34 +0330

    router finished

--- system/router/Routing.php
+++ system/router/Routing.php
@@ -2,6 +2,8 @@
 
 namespace System\router;
 
+use ReflectionMethod;
+
 class Routing
 {
 
@@ -13,7 +15,39 @@ class Routing
         $this -> current_route = explode('/' , $current_route);
     }
 
+    public function run()
+    {
+        $path = realpath(dirname(__FILE__) . "/../../application/controllers/" . $this->current_route[0] . ".php");
+        if(!file_exists($path))
+        {
+            echo "404 - file not exist";
+            exit;
+        }
+        require_once($path);        
 
+        sizeof($this->current_route) == 1 ? $method = "index" : $method = $this->current_route[1];
+
+        $class = "Application\Controllers\\" . $this->current_route[0];
+        $object = new $class();
+        if(method_exists($object , $method))
+        {
+            $reflection = new ReflectionMethod($class , $method);
+            $parameterCount = $reflection->getNumberOfParameters();
+            if($parameterCount <= count(array_slice($this->current_route , 2)))
+            
+                call_user_func_array(array($object , $method) , array_slice($this->current_route , 2));
+            
+            else
+            {
+                echo "404 - parameter Error!";
+            }
+        }
+        else
+        {
+            echo "404 - method not found!";
+        }
+
+    }
 
 
 
