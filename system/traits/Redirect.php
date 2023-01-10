d3c1e14a3688afbc21f459170885336c9603df24
mohammad <smirzaie9287@gmail.com>
2023-01-10 21:37:07 +0330

    traits added

---
+++ system/traits/Redirect.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace System\Traits;
+
+trait Redirect 
+{
+    protected function redirect($url)
+    {
+        $protocol = stripos($_SERVER['SERVER_PROTOCOL'] , 'https://') === true ? 'https://' : 'http://';
+        header("Location: ".$protocol.$_SERVER['HTTP_HOST']."/mvc/".$url);
+    }
+
+    protected function back()
+    {
+        $httpReferer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
+        if($httpReferer != null){
+        header("Location: ". $_SERVER['HTTP_REFERER']);
+        }
+        else
+        {
+        echo 'route not found';
+        }    
+    }
+
+
+
+
+}
