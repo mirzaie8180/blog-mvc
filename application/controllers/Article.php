1d5a349ead701747ea61591f57589694d3b63088
mohammad <smirzaie9287@gmail.com>
2023-01-15 14:25:15 +0330

    article and category for both controllers and models added

---
+++ application/controllers/Article.php
@@ -0,0 +1,50 @@
+<?php 
+
+namespace Application\Controllers;
+
+class Article extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update($id)
+    {
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ application/model/Article.php
@@ -0,0 +1,35 @@
+<?php 
+
+namespace Application\Model;
+
+class Article extends Model
+{
+
+    public function all()
+    {
+
+    }
+
+
+
+    public function find($id)
+    {
+        
+    }
+
+    public function insert($values)
+    {
+        
+    }
+
+    public function update($id,$values)
+    {
+        
+    }
+    public function delete($id)
+    {
+        
+    }
+
+
+}
