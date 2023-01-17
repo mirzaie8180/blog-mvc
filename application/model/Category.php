619e4bb7c2d46f0958ff255f48873eb98ddd93b9
mohammad <smirzaie9287@gmail.com>
2023-01-17 22:44:06 +0330

    category model added + something else

--- application/model/Category.php
+++ application/model/Category.php
@@ -15,26 +15,39 @@ class Category extends Model
 
     public function articles($cat_id)
     {
-        
+        $query = "SELECT * FROM `articles` WHERE cat_id = ? ";
+        $result = $this->query($query , array($cat_id))->fetch();
+        $this->closeConnection();
+        return $result;
     }
 
     public function find($id)
     {
-        
+        $query = "SELECT * FROM `categories` WHERE `id` = ? ";
+        $result = $this->query($query , array($id))->fetch();
+        $this->closeConnection();
+        return $result; 
     }
 
     public function insert($values)
     {
-        
+        $query = "INSERT INTO `categories` SET (`name`,`description`,`created_at`) VALUES (?,?,now() );";
+        $this->execute($query , array_values($values));
+        $this->closeConnection();
+      
     }
 
     public function update($id,$values)
     {
-        
+        $query = "UPDATE `categories` SET `name` = ? , `description` = ? , `updated_at` = now() WHERE `id` = ? ;";
+        $this->execute($query , array_merge( array_values($values , [$id])));
+        $this->closeConnection();
     }
     public function delete($id)
     {
-        
+        $query = "DELETE FROM `categories` WHERE `id` = ? ;";
+        $this->execute($query , [$id]);
+        $this->closeConnection();
     }
 
 
--- application/model/Model.php
+++ application/model/Model.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Application\Model;
 
@@ -11,17 +11,13 @@ class Model
 
     public function __construct()
     {
-        if(!isset($connection))
-        {
-            global $dbHost, $dbName, $dbUsername, $dbPassword; 
-            $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION , PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC , PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8");
+        if (!isset($connection)) {
+            global $dbHost, $dbName, $dbUsername, $dbPassword;
+            $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 
-            try
-            {
-                $this->connection = new PDO("mysql:host=".$dbHost.";dbname=".$dbName,$dbUsername,$dbPassword,$options);
-            }
-            catch(PDOException $e)
-            {
+            try {
+                $this->connection = new PDO("mysql:host=" . $dbHost . ";dbname=" . $dbName, $dbUsername, $dbPassword, $options);
+            } catch (PDOException $e) {
                 echo "There is some problem in connection: " . $e->getMessage();
             }
         }
@@ -32,44 +28,32 @@ class Model
         $this->closeConnection();
     }
 
-    protected function query($query , $values = null)
+    protected function query($query, $values = null)
     {
-        try
-        {
-        if($values == null)
-        {
-            return $this->connection->query($query);
-        }   
-        else
-        {
-            $stmt = $this->connection->prepare($query);
-            $stmt->execute($values);
-            return $stmt;
-        }
-        }
-        catch (PDOException $e)
-        {
+        try {
+            if ($values == null) {
+                return $this->connection->query($query);
+            } else {
+                $stmt = $this->connection->prepare($query);
+                $stmt->execute($values);
+                return $stmt;
+            }
+        } catch (PDOException $e) {
             echo "This is some problem in connection: " . $e->getMessage();
         }
     }
 
-    protected function execute($query , $values = null)
+    protected function execute($query, $values = null)
     {
-        try
-        {
-        if($values == null)
-        {
-            $this->connection->exec($query);
-        }
-        else
-        {
-            $stmt = $this->connection->prepare($query);
-            $stmt->execute($values);
-        }
-        return true;
-        }
-        catch (PDOException $e)
-        {
+        try {
+            if ($values == null) {
+                $this->connection->exec($query);
+            } else {
+                $stmt = $this->connection->prepare($query);
+                $stmt->execute($values);
+            }
+            return true;
+        } catch (PDOException $e) {
             echo "This is some problem in connection: " . $e->getMessage();
             return false;
         }
