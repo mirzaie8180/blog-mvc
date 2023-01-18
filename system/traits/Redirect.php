4c9a62c74532385e5ebc1b45cc614bf506724d76
mohammad <smirzaie9287@gmail.com>
2023-01-18 21:23:09 +0330

    category controller added

--- system/traits/Redirect.php
+++ system/traits/Redirect.php
@@ -1,28 +1,22 @@
-<?php 
+<?php
 
 namespace System\Traits;
 
-trait Redirect 
+trait Redirect
 {
     protected function redirect($url)
     {
-        $protocol = stripos($_SERVER['SERVER_PROTOCOL'] , 'https://') === true ? 'https://' : 'http://';
-        header("Location: ".$protocol.$_SERVER['HTTP_HOST']."/mvc/".$url);
+        $protocol = stripos($_SERVER['SERVER_PROTOCOL'], 'https://') === true ? 'https://' : 'http://';
+        header("Location: " . $protocol . $_SERVER['HTTP_HOST'] . "/mvc/" . $url);
     }
 
     protected function back()
     {
         $httpReferer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
-        if($httpReferer != null){
-        header("Location: ". $_SERVER['HTTP_REFERER']);
+        if ($httpReferer != null) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+        } else {
+            echo 'route not found';
         }
-        else
-        {
-        echo 'route not found';
-        }    
     }
-
-
-
-
 }
