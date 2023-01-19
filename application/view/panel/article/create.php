6a44f741358ff96ac4a0df29f2646c9e859c7854
mohammad <smirzaie9287@gmail.com>
2023-01-19 13:13:19 +0330

    create and edit (articlePanel) added

--- application/view/panel/article/create.php
+++ application/view/panel/article/create.php
@@ -1,20 +1,21 @@
 <?php $this->include("panel.layouts.header"); ?>
 
-<form>
+<form action="<?php $this->url('article/store'); ?>" method="post">
     <section class="form-group">
         <label for="title">Title</label>
-        <input type="text" class="form-control" id="title" placeholder="title ...">
+        <input type="text" class="form-control" id="title" name="title" placeholder="title ...">
     </section>
     <section class="form-group">
         <label for="cat_id">Category</label>
-        <select class="form-control" id="cat_id">
-            <option value="1">Sport</option>
-            <option value="2">News</option>
+        <select class="form-control" id="cat_id" name="cat_id">
+            <?php foreach ($categories as $category) { ?>
+                <option value="<?= $category['id'] ?>"><?= $category['name'] ?></option>
+            <?php } ?>
         </select>
     </section>
     <section class="form-group">
         <label for="body">Body</label>
-        <textarea class="form-control" id="body" rows="5" placeholder="body ..."></textarea>
+        <textarea class="form-control" id="body" name="body" rows="5" placeholder="body ..."></textarea>
     </section>
     <button type="submit" class="btn btn-primary">Create</button>
 </form>
--- application/view/panel/article/edit.php
+++ application/view/panel/article/edit.php
@@ -1,20 +1,21 @@
 <?php $this->include("panel.layouts.header"); ?>
 
-<form>
+<form action="<?php $this->url('article/update/' . $article['id']); ?>" method="post">
     <section class="form-group">
         <label for="title">Title</label>
-        <input type="text" class="form-control" id="title" placeholder="title ...">
+        <input type="text" class="form-control" id="title" name="title" value="<?= $article['title']; ?>">
     </section>
     <section class="form-group">
         <label for="cat_id">Category</label>
-        <select class="form-control" id="cat_id">
-            <option value="1">Sport</option>
-            <option value="2">News</option>
+        <select class="form-control" id="cat_id" name="cat_id">
+            <?php foreach ($categories as $category) { ?>
+                <option value="<?= $category['id'] ?>" <?php if ($article['cat_id'] == $category['id']) echo 'selected'; ?>><?= $category['name'] ?></option>
+            <?php } ?>
         </select>
     </section>
     <section class="form-group">
         <label for="body">Body</label>
-        <textarea class="form-control" id="body" rows="5" placeholder="body ..."></textarea>
+        <textarea class="form-control" id="body" name="body" value="<?= $article['body']; ?>" rows="5"></textarea>
     </section>
     <button type="submit" class="btn btn-primary">Update</button>
 </form>
