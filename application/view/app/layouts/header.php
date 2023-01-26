5587dae8d12057f554b7ff3712f30d044b0d2386
mohammad <smirzaie9287@gmail.com>
2023-01-26 08:17:48 +0330

    last update

--- application/view/app/layouts/header.php
+++ application/view/app/layouts/header.php
@@ -11,7 +11,7 @@
 <section id="app">
 
     <nav class="navbar navbar-expand-lg navbar-dark  bg-blue">
-        <a class="navbar-brand" href="<?= $this->asset('panel'); ?>">MVC tutorial</a>
+        <a class="navbar-brand" href="<?= $this->url('panel'); ?>">MVC tutorial</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
